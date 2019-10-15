040e7ca7dd14c99bb5ba6745d9a1bc2135c9c80e
RLWS靜 <1600510083@qq.com>
2019-10-15 16:52:56 +0800

    忘记咯

--- src/main/java/com/rlws/plant/platform/interceptor/UserInterceptor.java
+++ src/main/java/com/rlws/plant/platform/interceptor/UserInterceptor.java
@@ -8,8 +8,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
 
 @PropertySource("classpath:/plant-platform.properties")
 public class UserInterceptor implements HandlerInterceptor {
@@ -20,20 +22,26 @@ public class UserInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         User user = (User) httpServletRequest.getSession().getAttribute("user");
         if (user == null) {
-            String encodeURL = URLEncoder.encode(httpServletRequest.getRequestURL().toString(), "utf-8");
-            //String decodeURL = URLDecoder.decode(httpServletRequest.getRequestURL().toString(), "utf-8");
-//            httpServletResponse.sendRedirect("https://www.baidu.com");
-            httpServletResponse.sendRedirect("http://"+url + "/index?requestUrl=" + encodeURL);
+            Enumeration<String> paraNames = httpServletRequest.getParameterNames();
+            StringBuilder condition = new StringBuilder();
+            for (Enumeration<String> e = paraNames; e.hasMoreElements(); ) {
+                String thisName = e.nextElement();
+                String thisValue = httpServletRequest.getParameter(thisName);
+                System.out.println("param的key:" + thisName + "--------------param的value:" + thisValue);
+                if ("token".equals(thisName)) {//token存在
+                    return true;
+                } else {//token不存在
+                    condition.append(thisName).append("=").append(thisValue).append("&");
+                }
+            }
+            System.out.println(httpServletRequest.getRequestURL().toString() + condition);
+            //请求的url编码
+            String encodeURL = URLEncoder.encode(httpServletRequest.getRequestURL().toString() + "?" + condition, "utf-8");
+            //页面重定向到注册中心
+            httpServletResponse.sendRedirect("http://" + url + "/index?requestUrl=" + encodeURL);
             System.out.println(url + "/index?requestUrl=" + encodeURL);
             return false;
-        }else {
-            System.out.println("not null");
         }
-//        Map<String, String[]> parameterMap = httpServletRequest.getParameterMap();
-//        String tokens = parameterMap.get("token")[0];
-//        if (tokens != null && tokens != "") {
-//
-//        }
         return true;
     }
 
